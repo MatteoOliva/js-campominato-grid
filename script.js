bca83aacb9bdc30830f320c58a6277e2c746f75b
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-18 17:31:35 +0100

    feat: aggiunto le funzioni per generare la griglia con celle all caricamento della pagina

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const  gridElement = document.getElementById('grid');
 const  playButton = document.getElementById('play');
 generateGrid(gridElement);
 
+function generateGrid(container) {
+    for (let i = 0; i < 100; i ++) {
+        const cellElement = generateCell();
+        container.append(cellElement);
+    }
+}
+
+function generateCell()  {
+    const cell = document.createElement('div');
+    cell.classList.add('celle');
+    return cell;
+}
+
+
+
+
+
 
 // assegnare alle celle un numero da 1 a 100 progressivamente da disporre 10x10
 // Per ogni cella cliccata, essa diventa colore azzurro
