13975b6ed9aad30367d1277e1b123ac9ada2877e
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-18 17:01:35 +0100

    feat: aggiunto l id al button

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // creare un bottone che genera una griglia di gioco quadrata
+const  gridElement = document.getElementById('grid');
+const  playButton = document.getElementById('play');
+generateGrid(gridElement);
+
+
 // assegnare alle celle un numero da 1 a 100 progressivamente da disporre 10x10
 // Per ogni cella cliccata, essa diventa colore azzurro
 //   stampare in console il numero della cella cliccata
